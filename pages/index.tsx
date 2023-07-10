866d8de225a2316632cb9092de6436fc3906ddfb
Ole Vedeler <olevedeler@gmail.com>
2023-07-10 14:59:26 +0200

    Prettify and fixed typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,10 +17,21 @@ export default function Web() {
       <section className="bg-white dark:bg-gray-900">
         <div className="mx-auto grid max-w-screen-xl px-4 py-8 text-center lg:py-16">
           <div className="mx-auto place-self-center">
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">Oles eleven</h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">This website is a a challenge page where there will be posted 11 challenges for Ole and Benjamin to complete. </p>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">Everyone can participate in these challenges and send us an email to be shown on the leaderboard. For that challenge. Currently there is only one challenge, but more will be added after the previous one has been completed.</p>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">This is also a place to deploy our software projects to and will contain a lot of different projects.</p>
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none tracking-tight dark:text-white md:text-5xl xl:text-6xl">
+              Oles eleven
+            </h1>
+            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+              This website is a challenge page where there will be posted 11 challenges for Ole and Benjamin to
+              complete.{" "}
+            </p>
+            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+              Everyone can participate in these challenges and send us an email to be shown on the leaderboard. For that
+              challenge. Currently there is only one challenge, but more will be added after the previous one has been
+              completed.
+            </p>
+            <p className="mb-6 max-w-2xl font-light text-gray-500 dark:text-gray-400 md:text-lg lg:mb-8 lg:text-xl">
+              This is also a place to deploy our software projects to and will contain a lot of different projects.
+            </p>
           </div>
         </div>
       </section>
