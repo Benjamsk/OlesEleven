cb8cc2da2ea6a8bddfb13e4a1e5cc010c8c530c6
benjamsk <benjamin.s.kjaer@gmail.com>
2024-10-08 09:39:41 +0200

    Redo how things are generated and calculated

--- src/Pages/Tiles.tsx
+++ src/Pages/Tiles.tsx
@@ -1,52 +1,27 @@
 import { Box, Container } from '@mui/material';
 import React from 'react';
 
-class Connection {
-    public x: number;
-    public y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 export const Tiles = () => {
-    const width = 25;
-    const height = 25;
+    const width = 50;
+    const height = 50;
 
-    const connections = new Array(width).fill(null).map(() => new Array(height).fill(null).map(() => new Array<Connection>()));
-    
-    const horizontal = new Array<boolean>(width).fill(false).map(() => Math.random() < 0.5);
-    const vertical = new Array<boolean>(height).fill(false).map(() => Math.random() < 0.5);
+    const horizontal = new Array<boolean>(height - 1).fill(false).map(() => Math.random() < 0.5);
+    const vertical = new Array<boolean>(width - 1).fill(false).map(() => Math.random() < 0.5);
+    const grid = new Array(width).fill(null).map(() => new Array<boolean>(height).fill(false));
 
-    for (let x = 0; x < width; x++) {
-        for (let y = 0; y < height; y++) {
-            if (((horizontal[x] && y % 2 === 0) || (!horizontal[x] && y % 2 === 1)) && x > 0) {
-                connections[x][y].push(new Connection(x, y + 1));
-            }
-            if (((vertical[y] && x % 2 === 0) || (!vertical[y] && x % 2 === 1)) && y > 0) {
-                connections[x][y].push(new Connection(x + 1, y));
-            }
+ 
+    for(let y = 0; y < height; y++) {
+        if (y > 0) {
+            grid[0][y] = horizontal[y] ? !grid[0][y - 1] : grid[0][y - 1];
         }
-    }
 
-    const cells = new Array(width).fill(null).map(() => new Array<boolean>(height).fill(false));
+        for(let x = 1; x < width; x++) {
+            var leftValue = grid[x - 1][y];
 
-    cells[0][0] = Math.random() < 0.5;
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            if (x === 0 && y === 0) {
-                continue;
-            }
-
-            const previousCell = x === 0 ? cells[x][y - 1] : cells[x - 1][y];
-            const cellConnections = connections[x][y];
-    
-            if (x === 0) {
-                cells[x][y] = cellConnections.some(c => c.x === x + 1 && c.y === y) ? !previousCell : previousCell;
+            if (y % 2 == 0) {
+                grid[x][y] = vertical[x - 1] ? !leftValue : leftValue;
             } else {
-                cells[x][y] = cellConnections.some(c => c.x === x && c.y === y + 1) ? !previousCell : previousCell;
+                grid[x][y] = vertical[x - 1] ? leftValue : !leftValue;
             }
         }
     }
@@ -54,7 +29,7 @@ export const Tiles = () => {
     return (
         <Box sx={{ my: 4, display: 'flex', justifyContent: 'center'}}>
             <svg width={width * 16} height={height * 16}>
-                {cells.map((row, x) =>
+                {grid.map((row, x) =>
                     row.map((region, y) => (
                         <rect
                             key={`${x}-${y}`}
@@ -66,20 +41,6 @@ export const Tiles = () => {
                         />
                     ))
                 )}
-                {/*connections.map((row, x) =>
-                    row.map((cell, y) =>
-                        cell.map((connection, index) => (
-                            <line
-                                key={`${x}-${y}-${index}`}
-                                x1={x * 16}
-                                y1={y * 16}
-                                x2={connection.x * 16}
-                                y2={connection.y * 16}
-                                stroke="white"
-                            />
-                        ))
-                    )
-                )*/}
             </svg>
         </Box>
     );
